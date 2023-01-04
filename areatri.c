2e9df98e7072f47429af6901190d62834d75bd1a
meghana22wh1a1217 <121919376+meghana22wh1a1217@users.noreply.github.com>
2023-01-04 09:59:44 +0530

    Add files via upload

---
+++ areatri.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+float s,a;
+printf("\n give the value of side of triangle");
+scanf("float*");
+a=sqrt(3/4)*s*s;
+printf("\n area of triangle=%f",a);
+}
